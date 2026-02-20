155921e7ed9ae3ba77c91baf42e25b5d1e053a2a
Andres Vargas <alvargasrod@gmail.com>
2026-02-19 22:30:34 -0500

    Add validation card when user want to pubish your properties

--- src/main/java/com/affiliate/rentals/gydi/properties/application/usecase/PublishPropertyUseCaseImpl.java
+++ src/main/java/com/affiliate/rentals/gydi/properties/application/usecase/PublishPropertyUseCaseImpl.java
@@ -1,8 +1,10 @@
 package com.affiliate.rentals.gydi.properties.application.usecase;
 
+import com.affiliate.rentals.gydi.properties.domain.exception.PropertyCannotBePublishedException;
 import com.affiliate.rentals.gydi.properties.domain.model.Property;
 import com.affiliate.rentals.gydi.properties.domain.ports.in.PublishPropertyUseCase;
 import com.affiliate.rentals.gydi.properties.domain.ports.out.PropertyRepositoryPort;
+import com.affiliate.rentals.gydi.subscriptions.domain.ports.PaymentMethodRepositoryPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class PublishPropertyUseCaseImpl implements PublishPropertyUseCase {
 
     private final PropertyRepositoryPort propertyRepository;
+    private final PaymentMethodRepositoryPort paymentMethodRepository;
 
-    public PublishPropertyUseCaseImpl(PropertyRepositoryPort propertyRepository) {
+    public PublishPropertyUseCaseImpl(
+            PropertyRepositoryPort propertyRepository,
+            PaymentMethodRepositoryPort paymentMethodRepository) {
         this.propertyRepository = propertyRepository;
+        this.paymentMethodRepository = paymentMethodRepository;
     }
 
     @Override
@@ -23,6 +29,13 @@ public class PublishPropertyUseCaseImpl implements PublishPropertyUseCase {
 
         if (!property.isOwnedBy(command.requestingUserId())) {
             throw new SecurityException("User is not authorized to publish this property");
+        }
+
+        boolean hasPaymentMethod = paymentMethodRepository.hasActivePaymentMethod(command.requestingUserId());
+        if (!hasPaymentMethod) {
+            throw new PropertyCannotBePublishedException(
+                    "Debes agregar un método de pago antes de publicar tu propiedad"
+            );
         }
 
         property.publish();
