230fb42e6ba991d364fa070c56a556ec04666303
Andres Vargas <alvargasrod@gmail.com>
2026-05-07 15:17:23 -0500

    feat: relax currency validation from 6-currency @Pattern to @Size(3-10) in DTOs

--- src/main/java/com/affiliate/rentals/gydi/properties/application/dto/CreatePropertyRequest.java
+++ src/main/java/com/affiliate/rentals/gydi/properties/application/dto/CreatePropertyRequest.java
@@ -16,7 +16,7 @@ public record CreatePropertyRequest(
 
         @NotNull(message = "Price is required") @DecimalMin(value = "0.01", message = "Price must be greater than zero") BigDecimal pricePerNight,
 
-        @NotBlank(message = "Currency is required") @Pattern(regexp = "USD|EUR|MXN|COP|CAD|GBP", message = "Invalid currency") String currency,
+        @NotBlank(message = "Currency is required") @Size(min = 3, max = 10, message = "Currency code must be 3-10 characters") String currency,
 
         @DecimalMin(value = "0.01", message = "Sale price must be greater than zero") BigDecimal salePrice,
 
